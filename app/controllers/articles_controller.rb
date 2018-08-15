fcf97235c4c2fdf80041dd9bad2412c2cbb29021
Harry Ryder <harrymryder@gmail.com>
2018-08-15 10:43:41 +0100

    Articles rails

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
+
+  def index
+    @articles = Article.all
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+end
